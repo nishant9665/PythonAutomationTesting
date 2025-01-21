9def30ead6dccba04730f726f03f7f08d91e6cdd
nishant9665 <nishant9665@gmail.com>
2025-01-21 14:03:57 +0530

    parallel execution code

---
+++ Pytest-SeleniumTutorials/test_AllBrowerWithParallelExecution.py
@@ -0,0 +1,207 @@
+from selenium import webdriver
+def test_allBrowser(cmdopt):
+    #browserName = input("enter the name :")
+    if cmdopt=="chrome":
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+        print(driver.title)
+        driver.quit()
+    elif cmdopt=="firefox":
+        driver = webdriver.Firefox()
+        driver.maximize_window()
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+        print(driver.title)
+        driver.quit()
+    else:
+        driver = webdriver.Ie()
+        driver.maximize_window()
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+        print(driver.title)
+        driver.quit()
+def test_allBrowser2(cmdopt):
+    #browserName = input("enter the name :")
+    if cmdopt=="chrome":
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+        print(driver.title)
+        driver.quit()
+    elif cmdopt=="firefox":
+        driver = webdriver.Firefox()
+        driver.maximize_window()
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+        print(driver.title)
+        driver.quit()
+    else:
+        driver = webdriver.Ie()
+        driver.maximize_window()
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+        print(driver.title)
+        driver.quit()
+def test_allBrowser3(cmdopt):
+    #browserName = input("enter the name :")
+    if cmdopt=="chrome":
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+        print(driver.title)
+        driver.quit()
+    elif cmdopt=="firefox":
+        driver = webdriver.Firefox()
+        driver.maximize_window()
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+        print(driver.title)
+        driver.quit()
+    else:
+        driver = webdriver.Ie()
+        driver.maximize_window()
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+        print(driver.title)
+        driver.quit()
+def test_allBrowser4(cmdopt):
+    #browserName = input("enter the name :")
+    if cmdopt=="chrome":
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+        print(driver.title)
+        driver.quit()
+    elif cmdopt=="firefox":
+        driver = webdriver.Firefox()
+        driver.maximize_window()
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+        print(driver.title)
+        driver.quit()
+    else:
+        driver = webdriver.Ie()
+        driver.maximize_window()
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+        print(driver.title)
+        driver.quit()
+def test_allBrowser5(cmdopt):
+    #browserName = input("enter the name :")
+    if cmdopt=="chrome":
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+        print(driver.title)
+        driver.quit()
+    elif cmdopt=="firefox":
+        driver = webdriver.Firefox()
+        driver.maximize_window()
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+        print(driver.title)
+        driver.quit()
+    else:
+        driver = webdriver.Ie()
+        driver.maximize_window()
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+        print(driver.title)
+        driver.quit()
+def test_allBrowser6(cmdopt):
+    #browserName = input("enter the name :")
+    if cmdopt=="chrome":
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+        print(driver.title)
+        driver.quit()
+    elif cmdopt=="firefox":
+        driver = webdriver.Firefox()
+        driver.maximize_window()
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+        print(driver.title)
+        driver.quit()
+    else:
+        driver = webdriver.Ie()
+        driver.maximize_window()
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+        print(driver.title)
+        driver.quit()
+def test_allBrowser7(cmdopt):
+    #browserName = input("enter the name :")
+    if cmdopt=="chrome":
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+        print(driver.title)
+        driver.quit()
+    elif cmdopt=="firefox":
+        driver = webdriver.Firefox()
+        driver.maximize_window()
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+        print(driver.title)
+        driver.quit()
+    else:
+        driver = webdriver.Ie()
+        driver.maximize_window()
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+        print(driver.title)
+        driver.quit()
+def test_allBrowser8(cmdopt):
+    #browserName = input("enter the name :")
+    if cmdopt=="chrome":
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+        print(driver.title)
+        driver.quit()
+    elif cmdopt=="firefox":
+        driver = webdriver.Firefox()
+        driver.maximize_window()
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+        print(driver.title)
+        driver.quit()
+    else:
+        driver = webdriver.Ie()
+        driver.maximize_window()
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+        print(driver.title)
+        driver.quit()
+
+def test_allBrowser9(cmdopt):
+    #browserName = input("enter the name :")
+    if cmdopt=="chrome":
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+        print(driver.title)
+        driver.quit()
+    elif cmdopt=="firefox":
+        driver = webdriver.Firefox()
+        driver.maximize_window()
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+        print(driver.title)
+        driver.quit()
+    else:
+        driver = webdriver.Ie()
+        driver.maximize_window()
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+        print(driver.title)
+        driver.quit()
+
+def test_allBrowser10(cmdopt):
+    #browserName = input("enter the name :")
+    if cmdopt=="chrome":
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+        print(driver.title)
+        driver.quit()
+    elif cmdopt=="firefox":
+        driver = webdriver.Firefox()
+        driver.maximize_window()
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+        print(driver.title)
+        driver.quit()
+    else:
+        driver = webdriver.Ie()
+        driver.maximize_window()
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+        print(driver.title)
+        driver.quit()
+
+
+
+
